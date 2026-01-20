dd65c4468b5006f11eb88887c58779faba84b208
Mauro <maurogt.dd@gmail.com>
2026-01-20 16:45:24 -0300

    avances panel gordos GANADERO

---
+++ vistas/modulos/resumen.php
@@ -0,0 +1,363 @@
+<?php
+// Vista: Resumen de oferta (7 tablas + tabla compuesta + gráfico)
+?>
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>Resumen <small>Oferta de Gordos</small></h1>
+    <ol class="breadcrumb">
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      <li class="active">Resumen</li>
+    </ol>
+  </section>
+
+  <section class="content">
+    <!-- 7 tablas -->
+    <div class="row">
+      <!-- Grupo NOVILLOS: Exportación, Campo-Pastoreo, Mercado Interno -->
+      <div class="col-lg-9 col-md-12">
+        <div class="box box-primary">
+          <div class="box-header with-border"><h3 class="box-title">NOVILLOS</h3></div>
+          <div class="box-body" style="padding-bottom:0;">
+            <div class="row">
+              <!-- 1: Exportación -->
+              <div class="col-lg-4 col-md-4">
+                <div class="box box-default" style="margin-bottom:10px;">
+                  <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Exportación</h3></div>
+                  <div class="box-body no-padding">
+                    <table class="table table-condensed table-bordered" style="margin:0;">
+                      <thead>
+                        <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                      </thead>
+                      <tbody id="tbl-exportacion"></tbody>
+                      <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-exportacion">0</th></tr></tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <!-- 2: Campo - Pastoreo -->
+              <div class="col-lg-4 col-md-4">
+                <div class="box box-default" style="margin-bottom:10px;">
+                  <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Campo - Pastoreo</h3></div>
+                  <div class="box-body no-padding">
+                    <table class="table table-condensed table-bordered" style="margin:0;">
+                      <thead>
+                        <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                      </thead>
+                      <tbody id="tbl-campo"></tbody>
+                      <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-campo">0</th></tr></tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <!-- 3: Mercado Interno -->
+              <div class="col-lg-4 col-md-4">
+                <div class="box box-default" style="margin-bottom:10px;">
+                  <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Mercado Interno</h3></div>
+                  <div class="box-body no-padding">
+                    <table class="table table-condensed table-bordered" style="margin:0;">
+                      <thead>
+                        <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                      </thead>
+                      <tbody id="tbl-mi-novillos"></tbody>
+                      <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-mi-novillos">0</th></tr></tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 4: TOROS -->
+      <div class="col-lg-3 col-md-6">
+        <div class="box box-primary">
+          <div class="box-header with-border"><h3 class="box-title">TOROS</h3></div>
+          <div class="box-body" style="padding-bottom:0;">
+            <div class="box box-default" style="margin-bottom:10px;">
+              <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Mercado Interno</h3></div>
+              <div class="box-body no-padding">
+                <table class="table table-condensed table-bordered" style="margin:0;">
+                  <thead>
+                    <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                  </thead>
+                  <tbody id="tbl-toros-mi"></tbody>
+                  <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-toros-mi">0</th></tr></tfoot>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <!-- 5: VAQUILLONAS -->
+      <div class="col-lg-4 col-md-6">
+        <div class="box box-primary">
+          <div class="box-header with-border"><h3 class="box-title">VAQUILLONAS</h3></div>
+          <div class="box-body" style="padding-bottom:0;">
+            <div class="box box-default" style="margin-bottom:10px;">
+              <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Mercado Interno</h3></div>
+              <div class="box-body no-padding">
+                <table class="table table-condensed table-bordered" style="margin:0;">
+                  <thead>
+                    <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                  </thead>
+                  <tbody id="tbl-vq-mi"></tbody>
+                  <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-vq-mi">0</th></tr></tfoot>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 6-7: HOTELERIA (Novillitos + Vaquillonas) -->
+      <div class="col-lg-8 col-md-12">
+        <div class="box box-primary">
+          <div class="box-header with-border"><h3 class="box-title">HOTELERIA</h3></div>
+          <div class="box-body" style="padding-bottom:0;">
+            <div class="row">
+              <!-- Novillitos -->
+              <div class="col-lg-6 col-md-6">
+                <div class="box box-default" style="margin-bottom:10px;">
+                  <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Novillitos</h3></div>
+                  <div class="box-body no-padding">
+                    <table class="table table-condensed table-bordered" style="margin:0;">
+                      <thead>
+                        <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                      </thead>
+                      <tbody id="tbl-hotel-nt"></tbody>
+                      <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-hotel-nt">0</th></tr></tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Vaquillonas -->
+              <div class="col-lg-6 col-md-6">
+                <div class="box box-default" style="margin-bottom:10px;">
+                  <div class="box-header with-border"><h3 class="box-title" style="font-size:14px;">Vaquillonas</h3></div>
+                  <div class="box-body no-padding">
+                    <table class="table table-condensed table-bordered" style="margin:0;">
+                      <thead>
+                        <tr><th style="width:60%">Kg</th><th class="text-right">N° CAB</th></tr>
+                      </thead>
+                      <tbody id="tbl-vq-hotel"></tbody>
+                      <tfoot><tr><th class="text-right">TOTAL</th><th class="text-right" id="tot-vq-hotel">0</th></tr></tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Tabla mensual + gráfico -->
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="box box-default">
+          <div class="box-header with-border"><h3 class="box-title">Oferta mensual</h3></div>
+          <div class="box-body no-padding">
+            <div class="table-responsive">
+              <table class="table table-bordered table-striped" style="margin:0;">
+                <thead>
+                  <tr class="bg-gray">
+                    <th rowspan="2" style="vertical-align:middle;">Mes</th>
+                    <th colspan="4" class="text-center">EXPO</th>
+                    <th colspan="4" class="text-center">MI</th>
+                  </tr>
+                  <tr class="bg-gray-light">
+                    <th class="text-right">Total</th>
+                    <th class="text-right">Feedlot</th>
+                    <th class="text-right">Campo</th>
+                    <th class="text-right">Hotel</th>
+                    <th class="text-right">Total</th>
+                    <th class="text-right">NT</th>
+                    <th class="text-right">VQ</th>
+                    <th class="text-right">Hotel</th>
+                  </tr>
+                </thead>
+                <tbody id="tbl-mensual"></tbody>
+                <tfoot>
+                  <tr>
+                    <th class="text-right">TOTAL</th>
+                    <th class="text-right" id="mensual-expo"></th>
+                    <th class="text-right" id="mensual-expo-feed"></th>
+                    <th class="text-right" id="mensual-expo-campo"></th>
+                    <th class="text-right" id="mensual-expo-hotel"></th>
+                    <th class="text-right" id="mensual-mi"></th>
+                    <th class="text-right" id="mensual-mi-nt"></th>
+                    <th class="text-right" id="mensual-mi-vq"></th>
+                    <th class="text-right" id="mensual-mi-hotel"></th>
+                  </tr>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-5">
+        <div class="box box-default">
+          <div class="box-header with-border"><h3 class="box-title">Oferta de Gordos</h3></div>
+          <div class="box-body">
+            <canvas id="chart-oferta" height="220"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+<script>
+(function(){
+  // Datos de ejemplo (fallback). Reemplazar por AJAX cuando esté disponible.
+  const data = {
+    superiores: {
+      exportacion: {
+        filas: [
+          {kg:'menos 300', cab:227}, {kg:'300 - 330', cab:169}, {kg:'330 - 360', cab:167},
+          {kg:'360 - 390', cab:213}, {kg:'390 - 420', cab:384}, {kg:'420 - 450', cab:285},
+          {kg:'450 a 480', cab:50}, {kg:'480 a 490', cab:115}, {kg:'MÁS 490', cab:24}
+        ]
+      },
+      campo: {
+        filas: [
+          {kg:'menos 300', cab:230}, {kg:'300 - 330', cab:192}, {kg:'330 - 360', cab:179},
+          {kg:'360 - 390', cab:82}, {kg:'390 - 420', cab:27}, {kg:'420 - 450', cab:21},
+          {kg:'450 a 460', cab:0}, {kg:'460 a 490', cab:0}, {kg:'MÁS 490', cab:1}
+        ]
+      },
+      mi_novillos: {
+        filas: [
+          {kg:'menos 300', cab:9}, {kg:'300 - 330', cab:13}, {kg:'330 - 360', cab:11},
+          {kg:'360 - 390', cab:29}, {kg:'390 - 420', cab:26}, {kg:'420 - 450', cab:0},
+          {kg:'450 a 480', cab:15}, {kg:'480 a 510', cab:11}, {kg:'MÁS 510', cab:1}
+        ]
+      },
+      toros_mi: {
+        filas: [
+          {kg:'menos 300', cab:9}, {kg:'300 - 330', cab:1}, {kg:'330 - 360', cab:11},
+          {kg:'360 - 390', cab:5}, {kg:'390 - 420', cab:1}, {kg:'420 a 450', cab:0},
+          {kg:'450 a 480', cab:0}, {kg:'480 a 510', cab:0}, {kg:'MÁS 510', cab:0}
+        ]
+      },
+      vq_mi: {
+        filas: [
+          {kg:'menos 180', cab:9}, {kg:'180 a 210', cab:18}, {kg:'210 - 240', cab:26},
+          {kg:'240 - 270', cab:37}, {kg:'270 - 300', cab:60}, {kg:'300 a 320', cab:168},
+          {kg:'320 a 350', cab:183}, {kg:'350 a 380', cab:97}, {kg:'MÁS 380', cab:76}
+        ]
+      },
+      hotel_nt: {
+        filas: [
+          {kg:'menos 300', cab:63}, {kg:'300 - 330', cab:20}, {kg:'330 - 360', cab:30},
+          {kg:'360 - 390', cab:3}, {kg:'390 - 420', cab:0}, {kg:'420 a 450', cab:8},
+          {kg:'450 a 480', cab:0}, {kg:'480 a 510', cab:0}, {kg:'MÁS 510', cab:0}
+        ]
+      },
+      vq_hotel: {
+        filas: [
+          {kg:'menos 180', cab:0}, {kg:'180 a 210', cab:12}, {kg:'210 - 240', cab:60},
+          {kg:'240 - 270', cab:102}, {kg:'270 - 300', cab:59}, {kg:'300 a 330', cab:9},
+          {kg:'330 a 350', cab:6}, {kg:'350 a 380', cab:2}, {kg:'MÁS 380', cab:2}
+        ]
+      }
+    },
+    mensual: [
+      {mes:'ENE', expo:{total:189, feedlot:189, campo:0, hotel:0}, mi:{total:337, nt:61, vq:259, hotel:17}},
+      {mes:'FEB', expo:{total:669, feedlot:669, campo:0, hotel:0}, mi:{total:407, nt:66, vq:260, hotel:81}},
+      {mes:'MAR', expo:{total:497, feedlot:382, campo:115, hotel:0}, mi:{total:469, nt:44, vq:243, hotel:182}},
+      {mes:'ABR', expo:{total:540, feedlot:169, campo:371, hotel:0}, mi:{total:529, nt:18, vq:336, hotel:175}},
+      {mes:'MAY', expo:{total:342, feedlot:227, campo:115, hotel:0}, mi:{total:18, nt:0, vq:18, hotel:0}},
+      {mes:'JUN', expo:{total:115, feedlot:0, campo:115, hotel:0}, mi:{total:9, nt:0, vq:9, hotel:0}},
+    ]
+  };
+
+  function fillSimpleTable(rows, bodySel, totalSel){
+    let total = 0;
+    const tbody = document.getElementById(bodySel);
+    tbody.innerHTML = '';
+    rows.forEach(r => {
+      total += Number(r.cab)||0;
+      const tr = document.createElement('tr');
+      tr.innerHTML = `<td>${r.kg}</td><td class="text-right">${Number(r.cab).toLocaleString('es-AR')}</td>`;
+      tbody.appendChild(tr);
+    });
+    document.getElementById(totalSel).innerText = total.toLocaleString('es-AR');
+  }
+
+  // Render 7 tablas
+  fillSimpleTable(data.superiores.exportacion.filas, 'tbl-exportacion', 'tot-exportacion');
+  fillSimpleTable(data.superiores.campo.filas, 'tbl-campo', 'tot-campo');
+  fillSimpleTable(data.superiores.mi_novillos.filas, 'tbl-mi-novillos', 'tot-mi-novillos');
+  fillSimpleTable(data.superiores.toros_mi.filas, 'tbl-toros-mi', 'tot-toros-mi');
+  fillSimpleTable(data.superiores.vq_mi.filas, 'tbl-vq-mi', 'tot-vq-mi');
+  fillSimpleTable(data.superiores.hotel_nt.filas, 'tbl-hotel-nt', 'tot-hotel-nt');
+  fillSimpleTable(data.superiores.vq_hotel.filas, 'tbl-vq-hotel', 'tot-vq-hotel');
+
+  // Tabla mensual
+  const tbodyMensual = document.getElementById('tbl-mensual');
+  let totExpo=0, totExpoFeed=0, totExpoCampo=0, totExpoHotel=0,
+      totMi=0, totMiNt=0, totMiVq=0, totMiHotel=0;
+  data.mensual.forEach(r => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = [
+      `<td>${r.mes}</td>`,
+      `<td class='text-right'>${r.expo.total.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.expo.feedlot.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.expo.campo.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.expo.hotel.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.mi.total.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.mi.nt.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.mi.vq.toLocaleString('es-AR')}</td>`,
+      `<td class='text-right'>${r.mi.hotel.toLocaleString('es-AR')}</td>`
+    ].join('');
+    tbodyMensual.appendChild(tr);
+
+    totExpo += r.expo.total; totExpoFeed += r.expo.feedlot; totExpoCampo += r.expo.campo; totExpoHotel += r.expo.hotel;
+    totMi += r.mi.total; totMiNt += r.mi.nt; totMiVq += r.mi.vq; totMiHotel += r.mi.hotel;
+  });
+  document.getElementById('mensual-expo').textContent = totExpo.toLocaleString('es-AR');
+  document.getElementById('mensual-expo-feed').textContent = totExpoFeed.toLocaleString('es-AR');
+  document.getElementById('mensual-expo-campo').textContent = totExpoCampo.toLocaleString('es-AR');
+  document.getElementById('mensual-expo-hotel').textContent = totExpoHotel.toLocaleString('es-AR');
+  document.getElementById('mensual-mi').textContent = totMi.toLocaleString('es-AR');
+  document.getElementById('mensual-mi-nt').textContent = totMiNt.toLocaleString('es-AR');
+  document.getElementById('mensual-mi-vq').textContent = totMiVq.toLocaleString('es-AR');
+  document.getElementById('mensual-mi-hotel').textContent = totMiHotel.toLocaleString('es-AR');
+
+  // Gráfico EXPO vs MI
+  try {
+    const ctx = document.getElementById('chart-oferta').getContext('2d');
+    const labels = data.mensual.map(m => m.mes);
+    const expo = data.mensual.map(m => m.expo.total);
+    const mi = data.mensual.map(m => m.mi.total);
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {label: 'EXPO', data: expo, borderColor: 'rgba(54,162,235,1)', backgroundColor: 'rgba(54,162,235,0.1)', fill: false, tension: 0.2},
+          {label: 'MI', data: mi, borderColor: 'rgba(255,99,132,1)', backgroundColor: 'rgba(255,99,132,0.1)', fill: false, tension: 0.2}
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: { yAxes: [{ ticks: { beginAtZero: true } }] },
+        legend: { display: true }
+      }
+    });
+  } catch (e) {
+    console.warn('Chart no disponible:', e);
+  }
+})();
+</script>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -200,6 +200,7 @@ CUERPO DOCUMENTO
           $_GET["ruta"] == "datos-compras" ||
           $_GET["ruta"] == "datos-muertes" ||
           $_GET["ruta"] == "panelControl" ||
+          $_GET["ruta"] == "resumen" ||
           $_GET["ruta"] == "resumenConversion" ||
           $_GET["ruta"] == "piri" ||
           $_GET["ruta"] == "diasPastoreo" ||
