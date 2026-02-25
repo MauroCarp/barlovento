a70c51fd0672a4ff9e49200563c602b94a065cf0
Mauro <maurogt.dd@gmail.com>
2026-02-25 08:34:02 -0300

    limpio console.logs

--- vistas/js/menu.js
+++ vistas/js/menu.js
@@ -10,18 +10,10 @@ var btnCompras = document.getElementById('btnCompras');
 var btnVentas = document.getElementById('btnVentas');
 var btnMuertes = document.getElementById('btnMuertes');
 
-$('#btnCompras').on('click',()=>{
-
-    console.log('asdasd');
-
-    
-});
-
 
 btnVentas.addEventListener('click',function(){    
 
     localStorage.removeItem("rango");
-console.log('asdasd');
 
     $('#daterange-btn').html('<span><i class="fa fa-calendar"></i> Rango de fecha </span><i class="fa fa-caret-down"></i>');
 
--- vistas/js/panelControl.js
+++ vistas/js/panelControl.js
@@ -360,7 +360,7 @@ $('.tabsPanelControl').on('click',function(){
 ZOOM GRAFICOS
 =============================================*/
 $('.zoomGraficos').each(function(){
-console.log($(this))
+
   $(this)[0].addEventListener('click',function(){
 
     let id = $(this).attr('data-id')
